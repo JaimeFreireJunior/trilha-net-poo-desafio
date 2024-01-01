fd9748a872048c1bb02b31fab8b518da5ea73cc8
Jaime Freire Junior <jaimefreire.junior@outlook.com>
2024-01-01 17:15:51 -0300

    Entrega do desafio feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
+// Implementado!!!!
+Console.WriteLine("Aparelho Nokia");
+Nokia celular1 = new Nokia(numero: "999999999", modelo: "Modelo Nokia 1", imei: "1111111111", memoria: 16);
+celular1.Ligar();
+celular1.ReceberLigacao();
+celular1.InstalarAplicativo("WhatsApp");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Console.WriteLine("Aparelho Iphone");
+Iphone celular2 = new Iphone(numero: "98888-7777", modelo: "Iphone13", imei: "2222222222", memoria: 64);
+celular2.Ligar();
+celular2.ReceberLigacao();
+celular2.InstalarAplicativo("Telegram");
